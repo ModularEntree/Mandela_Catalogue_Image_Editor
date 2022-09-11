1bf2cb64daf9979efa3afebb81fcece6a1fee636
Modular <filip.bednarglados@gmail.com>
2022-09-11 12:59:42 +0200

    complete basic function of the program

--- Image Editor/main.py
+++ Image Editor/main.py
@@ -1,21 +1,37 @@
 # import libraries
 # from tkinter import * 
 import tkinter as tk
+import os
+from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
-
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 win = tk.Tk()
 betterWin1 = tk.Canvas(win, width=400, height=300)
 betterWin1.pack()
 entry1 = tk.Entry (win)
 betterWin1.create_window(200, 140, window=entry1)
-def getPath ():
-    path = entry1.get()
-    img = Image.open('')
+entry2 = tk.Entry (win)
+betterWin1.create_window(200, 160, window=entry2)
+def ImageEditing ():
+    pathImg = entry1.get()
+    name = entry2.get()
+    img = Image.open(os.path.join(__location__, "edit_img.png"))
+    ManCatFont = ImageFont.truetype(os.path.join(__location__, "coolvetica rg.ttf"), 52)
+    ImageDraw.Draw(img).text((730, 565), "bad decision, " + name, font=ManCatFont)
+    img = img.save("final.png")
+buttonSubmit = tk.Button(text='Submit', command=ImageEditing)
+betterWin1.create_window(200, 180, window=buttonSubmit)
 win.mainloop()
 
 '''
-
+    ManCatFont = Font(
+        family="Coolvetica",
+        size=49,
+        weight="bold",
+        underline=0,
+        overstrike=0,
+    )
 # open image
 img = Image.open('/home/ubuntu/image.png')
 
