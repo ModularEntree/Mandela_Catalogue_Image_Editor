51d1c2a4edc12cdd48fea22b96a9083b7110c469
Modular <filip.bednarglados@gmail.com>
2022-09-11 11:05:38 +0200

    python testing

--- Image Editor/main.py
+++ Image Editor/main.py
@@ -1,1 +1,28 @@
-print("Hello World")
+# import libraries
+# from tkinter import * 
+import tkinter as tkinter
+from PIL import Image
+from PIL import ImageDraw
+
+
+
+'''
+okno = tkinter.Tk()
+def ahoj():  #funkce volaná po stisku tlačítka
+    print("Ahoj svete")
+button = tkinter.Button(okno, command=ahoj, text="Text tohoto tlačítka")  #tvorba tlačítka
+button.pack()  #zobrazení tlačítka
+okno.mainloop()  #spuštění vyhodnocovací smyčky
+
+# open image
+img = Image.open('/home/ubuntu/image.png')
+
+# draw image object
+I1 = ImageDraw.Draw(img)
+
+# add text to image
+I1.text((28, 36), "hello world", fill=(255, 0, 0))
+
+# save image
+img.save("/home/ubuntu/image.png")
+'''
