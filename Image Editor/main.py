aefd96be0d3a91dea37d2f33cc377cd0eca11bf0
Modular <filip.bednarglados@gmail.com>
2022-09-11 17:27:07 +0200

    complete fix!

--- Image Editor/main.py
+++ Image Editor/main.py
@@ -1,17 +1,19 @@
 # imports
 import tkinter as tk
 import os
+import pygame
 from turtle import width
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
+from PIL import ImageTk
 # path of py file declaration
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 
 # window config
 win = tk.Tk()
 win.title("Mandela Catalogue Image Editor")
-win.iconbitmap(os.path.join(__location__, "intruder.ico"))
+win.iconbitmap(os.path.join(__location__, "Intruder.ico"))
 
 # config of canvas
 betterWin1 = tk.Canvas(win, width=400, height=300)
@@ -34,10 +36,29 @@ def ImageEditing ():
     center = draw.textlength(text1, font=ManCatFont)
     draw.text((((1439-center)/2)+20, 565), text1, font=ManCatFont)
     img = img.save("final.png")
-    betterWin2 = tk.Toplevel(tk.Canvas(win, width=400, height=300))
+
+# scary function or something idk i just need to practice this shit
+def IntruderAlert ():
+    win2 = tk.Toplevel(win)
+    win2.title("Mandela Catalogue Image Editor")
+    win2.iconbitmap(os.path.join(__location__, "Intruder.ico"))
+    win2.geometry("902x688")
+    global imgbg
+    imgBg = Image.open(os.path.join(__location__, "Intruder.png")).resize((902, 688))
+    imgbg = ImageTk.PhotoImage(imgBg)
+    bg_label = tk.Label(win2, image=imgbg)
+    bg_label.place(x=0, y=0, relwidth=1, relheight=1)
+    win2.update()
+    pygame.mixer.init()
+    pygame.mixer.music.load(os.path.join(__location__, "Intruder.wav"))
+    pygame.mixer.music.play()
+    pygame.mixer.init()
+    pygame.mixer.music.load(os.path.join(__location__, "TheIntruderAlert.wav"))
+    pygame.mixer.music.play()
+    win2.mainloop()
 
 # button config
-buttonSubmit = tk.Button(text='Submit', command=ImageEditing)
+buttonSubmit = tk.Button(text='Submit', command=lambda: [ImageEditing(), IntruderAlert()])
 betterWin1.create_window(200, 170, window=buttonSubmit)
 
 # looping window
